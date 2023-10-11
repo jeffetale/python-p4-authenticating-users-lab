7cb38254ce93f6d2e9384e043db67ef846766073
jeff <jeff.etale@student.moringaschool.com>
2023-10-12 01:59:09 +0300

    done lab

--- server/app.py
+++ server/app.py
@@ -7,9 +7,9 @@ from flask_restful import Api, Resource
 from models import db, Article, User
 
 app = Flask(__name__)
-app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = b"Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
@@ -18,40 +18,74 @@ db.init_app(app)
 
 api = Api(app)
 
-class ClearSession(Resource):
 
+class ClearSession(Resource):
     def delete(self):
-    
-        session['page_views'] = None
-        session['user_id'] = None
+        session["page_views"] = None
+        session["user_id"] = None
 
         return {}, 204
 
+
 class IndexArticle(Resource):
-    
     def get(self):
         articles = [article.to_dict() for article in Article.query.all()]
         return articles, 200
 
-class ShowArticle(Resource):
 
+class ShowArticle(Resource):
     def get(self, id):
         session['page_views'] = 0 if not session.get('page_views') else session.get('page_views')
         session['page_views'] += 1
 
         if session['page_views'] <= 3:
-
             article = Article.query.filter(Article.id == id).first()
-            article_json = jsonify(article.to_dict())
+            if article is None:
+                return {'message': 'Article not found'}, 404
 
-            return make_response(article_json, 200)
+            return article.to_dict(), 200
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
-api.add_resource(ClearSession, '/clear')
-api.add_resource(IndexArticle, '/articles')
-api.add_resource(ShowArticle, '/articles/<int:id>')
 
 
-if __name__ == '__main__':
+class Login(Resource):
+    def post(self):
+        username = request.json.get("username")
+        user = User.query.filter_by(username=username).first()
+        if not user:
+            return {"message": "User not found"}, 404
+
+        session["user_id"] = user.id
+        return user.to_dict(), 200
+
+
+class Logout(Resource):
+    def delete(self):
+        session.pop("user_id", None)
+        return {}, 204
+
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get("user_id")
+        if not user_id:
+            return {}, 401
+
+        user = User.query.get(user_id)
+        if not user:
+            return {"message": "User not found"}, 404
+
+        return user.to_dict(), 200
+
+
+api.add_resource(ClearSession, "/clear")
+api.add_resource(IndexArticle, "/articles")
+api.add_resource(ShowArticle, "/articles/<int:id>")
+api.add_resource(Login, "/login")
+api.add_resource(Logout, "/logout")
+api.add_resource(CheckSession, "/check_session")
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -2,14 +2,17 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
 
 db = SQLAlchemy(metadata=metadata)
 
+
 class Article(db.Model, SerializerMixin):
-    __tablename__ = 'articles'
+    __tablename__ = "articles"
 
     id = db.Column(db.Integer, primary_key=True)
     author = db.Column(db.String)
@@ -19,18 +22,27 @@ class Article(db.Model, SerializerMixin):
     minutes_to_read = db.Column(db.Integer)
     date = db.Column(db.DateTime, server_default=db.func.now())
 
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
 
     def __repr__(self):
-        return f'Article {self.id} by {self.author}'
+        return f"Article {self.id} by {self.author}"
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "content": self.content,
+            "author": self.author
+        }
+
 
 class User(db.Model, SerializerMixin):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True)
 
-    articles = db.relationship('Article', backref='user')
+    articles = db.relationship("Article", backref="user")
 
     def __repr__(self):
-        return f'User {self.username}, ID {self.id}'
+        return f"User {self.username}, ID {self.id}"
